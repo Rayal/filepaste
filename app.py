95de4664fd8303efdc1c29058580ecba51436157
Andreas Ragen Ayal <5462920+Rayal@users.noreply.github.com>
2020-11-18 01:33:28 +0200

    Works!

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-from flask import Flask
+from io import BytesIO
+
+from flask import Flask, request, redirect, flash, make_response, jsonify, send_file, render_template
+from werkzeug.utils import secure_filename
+
+from filestore_interface import create_file_store_interface
+
+DISALLOWED_EXTENSIONS = {'html', 'php'}
 
 app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 1024 ** 3  # 1 GB
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.split('.')[-1].lower() not in DISALLOWED_EXTENSIONS
 
 
 @app.route('/')
@@ -8,9 +20,55 @@ def hello_world():
     return 'Hello World!'
 
 
-@app.route('/upload', methods=['GET'])
+@app.route('/upload', methods=['GET', 'POST'])
 def upload():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash("No file found")
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash("No file found")
+            return redirect(request.url)
+        if file:
+            filename = secure_filename(file.filename)
+            response = make_response(jsonify({
+                    "message": "Whoops!"
+                }), 503)
+            with create_file_store_interface() as interface:
+                file_id = interface.insert_file(filename, file.read())
+                response = make_response(jsonify({
+                    "fileId": file_id
+                }), 201)
+            response.headers["Content-Type"] = "application/json"
+            return response
 
+    return """
+        <!doctype html>
+        <title>Upload new File</title>
+        <h1>Upload new File</h1>
+        <form method=post enctype=multipart/form-data>
+          <input type=file name=file>
+          <input type=submit value=Upload>
+        </form>
+        """
+
+
+@app.route("/download/<file_id>")
+def download(file_id):
+    with create_file_store_interface() as interface:
+        filename, file_data = interface.get_file(file_id)
+        return send_file(
+            BytesIO(bytes(file_data)),
+            as_attachment=True,
+            attachment_filename=filename)
+
+
+@app.route("/files")
+def get_files():
+    with create_file_store_interface() as interface:
+        files = interface.get_files()
+        return render_template("files_list.html", files=files)
 
 
 if __name__ == '__main__':
---
+++ filestore_interface.py
@@ -0,0 +1,66 @@
+
+from os import environ
+from uuid import uuid4
+import psycopg2 as pg  # type: ignore
+
+
+def get_database_url() -> str:
+    return environ['DATABASE_URL']
+
+
+def get_database_password() -> str:
+    try:
+        return environ['DATABASE_PASSWORD']
+    except KeyError:
+        return ""
+
+
+class FileStoreInterface:
+    def _create_connection(self, **kwargs) -> None:
+        self._connection = pg.connect(self._database_url, sslmode='require', **kwargs)
+        self._cursor = self._connection.cursor()
+
+    def __init__(self, **kwargs):
+        self._connection = None
+        self._cursor = None
+        self._database_url = get_database_url()
+        self._create_connection(**kwargs)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print(exc_type, exc_val, exc_tb)
+        self._cursor.close()
+        self._connection.close()
+
+    def insert_file(self, filename: str, file_data: bytes) -> str:
+        uuid = str(uuid4())
+        query = "INSERT INTO paste (id, filename, file_data) VALUES (%(id)s, %(str)s, %(bytes)s);"
+        mapping = {
+            'id': uuid,
+            'str': filename,
+            'bytes': file_data
+        }
+        self._cursor.execute(query, mapping)
+        self._connection.commit()
+        return uuid
+
+    def get_file(self, file_id: str) -> (str, memoryview):
+        if file_id:
+            query = "SELECT filename, file_data FROM paste where id = %s;"
+            self._cursor.execute(query, (file_id,))
+            return self._cursor.fetchone()
+        raise FileNotFoundError(file_id)
+
+    def get_files(self) -> list:
+        query = "SELECT id, filename from paste;"
+        self._cursor.execute(query)
+        return self._cursor.fetchall()
+
+
+def create_file_store_interface() -> FileStoreInterface:
+    password = get_database_password()
+    if password:
+        return FileStoreInterface(password=password)
+    return FileStoreInterface()
